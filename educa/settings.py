9d63eb95c20e595310663754f18773fcabd8a06b
Restyled.io <commits@restyled.io>
2020-07-25 13:15:40 +0000

    Restyled by isort

--- educa/settings.py
+++ educa/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dotenv
-from dotenv import load_dotenv
 
+import dotenv
 from django.urls import reverse_lazy
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
